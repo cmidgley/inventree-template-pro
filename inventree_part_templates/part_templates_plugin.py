b836cfe4c66d49e19149ffbe0381f901e30bcd53
Chris Midgley <chris@koose.com>
2024-04-11 08:54:03 -0400

    Cleaned up button actions

--- inventree_part_templates/part_templates_plugin.py
+++ inventree_part_templates/part_templates_plugin.py
@@ -203,14 +203,14 @@ class PartTemplatesPlugin(PanelMixin, UrlsMixin, ReportMixin, SettingsMixin, Inv
                 'title': 'Part Templates',
                 'icon': 'fa-file-alt',
                 'content_template': 'part_templates/part_detail_panel.html',
-                'javascript': 'onPanelLoad();'
+                'javascript': 'onPartTemplatesPanelLoad();'
             })
         elif isinstance(view, CategoryDetail):
             panels.append({
                 'title': 'Part Templates',
                 'icon': 'fa-file-alt',
                 'content_template': 'part_templates/category_detail_panel.html',
-                'javascript': 'onPanelLoad();'
+                'javascript': 'onPartTemplatesPanelLoad();'
             })
 
         return panels

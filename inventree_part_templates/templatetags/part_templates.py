4e69c3e6eedac5a4affc1f21e63c0b82ae7a3119
cmidgley <chris@Koose.com>
2024-04-24 16:12:05 -0400

    Corrected import file and eliminate lint warnings

--- inventree_part_templates/templatetags/part_templates.py
+++ inventree_part_templates/templatetags/part_templates.py
@@ -1,5 +1,14 @@
-import explore
-import replace
-import stocklist
-import value_filters
-import part_context
+""" 
+    Main include file for all template tags and filters for the inventree-part-template plugin  
+
+    Copyright (c) 2024 Chris Midgley
+    License: MIT (see LICENSE file)
+"""
+
+from .explore import explore
+from .replace import replace
+from .stocklist import stocklist
+from .value_filters import scrub, value, item
+from .part_context import part_context
+
+__all__ = ['explore', 'replace', 'stocklist', 'scrub', 'value', 'item', 'part_context']

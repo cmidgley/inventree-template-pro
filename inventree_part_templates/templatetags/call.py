7557e2d2e42690e78181a93f95cfd6ce4ee45b6a
cmidgley <chris@Koose.com>
2024-04-29 11:24:05 -0400

    Simple_tag for 'call' to call methods with arguments

---
+++ inventree_part_templates/templatetags/call.py
@@ -0,0 +1,22 @@
+""" 
+    'explore' template tag for the inventree-part-template plugin  
+
+    Copyright (c) 2024 Chris Midgley
+    License: MIT (see LICENSE file)
+"""
+
+from .shared import register
+
+@register.simple_tag
+def call(obj, method_name, *args):
+    """
+    Call a method by name on an object with any number of arguments and return the result.
+
+    Example:
+    - {% call part "getRequiredParts" true %}
+    """
+    if hasattr(obj, method_name):
+        method = getattr(obj, method_name)
+        if callable(method):
+            return method(*args)
+    return None

f8198c85c7772ec3a161cf61f99264aad8e5d9d5
cmidgley <chris@Koose.com>
2024-04-29 12:22:58 -0400

    Extend call to support "as <var>" syntax

--- inventree_part_templates/templatetags/call.py
+++ inventree_part_templates/templatetags/call.py
@@ -7,16 +7,21 @@
 
 from .shared import register
 
-@register.simple_tag
-def call(obj, method_name, *args):
+@register.simple_tag(takes_context=True)
+def call(context, obj, method_name, *args, **kwargs):
     """
     Call a method by name on an object with any number of arguments and return the result.
 
     Example:
     - {% call part "getRequiredParts" true as required_parts %}
     """
+    result = None
     if hasattr(obj, method_name):
         method = getattr(obj, method_name)
         if callable(method):
-            return method(*args)
-    return None
+            result = method(*args)
+    varname = kwargs.get('as')
+    if varname:
+        context[varname] = result
+        return ''
+    return result

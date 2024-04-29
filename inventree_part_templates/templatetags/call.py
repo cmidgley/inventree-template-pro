7c2849dfb2559193e9951e67c40a8e3281afa501
cmidgley <chris@Koose.com>
2024-04-29 12:28:25 -0400

    Test code: Hack on call to direct call local getRequriedParts to debug why that call isn't working

--- inventree_part_templates/templatetags/call.py
+++ inventree_part_templates/templatetags/call.py
@@ -19,9 +19,27 @@ def call(context, obj, method_name, *args, **kwargs):
     if hasattr(obj, method_name):
         method = getattr(obj, method_name)
         if callable(method):
-            result = method(*args)
+            # result = method(*args)
+            result = getRequiredParts(obj, recursive=True, parts=None)
     varname = kwargs.get('as')
     if varname:
         context[varname] = result
         return ''
     return result
+
+def getRequiredParts(part, recursive=False, parts=None):
+    if parts is None:
+        parts = set()
+
+    bom_items = part.get_bom_items()
+
+    for bom_item in bom_items:
+        sub_part = bom_item.sub_part
+
+        if sub_part not in parts:
+            parts.add(sub_part)
+
+            if recursive:
+                sub_part.getRequiredParts(part, recursive=True, parts=parts)
+
+    return parts
